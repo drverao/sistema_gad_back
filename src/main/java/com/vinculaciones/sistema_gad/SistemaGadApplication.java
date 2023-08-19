2489987cda835424a91d4fe1b9149fdf569caf1c
dverao1997 <diego.r.vera.ortiz@gmail.com>
2023-08-19 16:00:12 -0500

    Primer commit

---
+++ src/main/java/com/vinculaciones/sistema_gad/SistemaGadApplication.java
@@ -0,0 +1,13 @@
+package com.vinculaciones.sistema_gad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinculaciones/sistema_gad/SistemaGadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinculaciones.sistema_gad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
